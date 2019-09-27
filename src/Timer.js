5b7c875b05d4fb412f1e9b4ec72f42345c6f89a2
Martha Sharpe <marthasharpe2020@gmail.com>
2019-09-27 11:00:11 -0400

    minutes and seconds from timeLeft

--- src/Timer.js
+++ src/Timer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Timer.css';
 
-const Timer = props => {    
+const Timer = props => {  
+
+    let time = props.timeLeft * 60;
+    let minutes = Math.floor(time / 60);
+    let seconds = (time % 60);
+
     return (
         <div className='timer-container'>
             <h2 id='timer-label'>{props.timerLabel} Time</h2>
-            {/* <h3 id='time-left'>{minutes}:{seconds}</h3> */}
+            <h3 id='time-left'>
+                {minutes < 10 ? ("0" + minutes).slice(-2) : minutes}:{seconds < 10 ? ("0" + seconds).slice(-2) : seconds}
+            </h3>
             <button id='start-stop'>Start/Stop</button>
         </div>
     )
